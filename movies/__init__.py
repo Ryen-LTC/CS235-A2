c7a402caf972643f0e81945f2f9199ef42218570
Ryen-LTC <740351000@qq.com>
2020-10-23 02:58:54 +1300

    Initialize Flask app

---
+++ movies/__init__.py
@@ -0,0 +1,31 @@
+"""Initialize Flask app."""
+
+import os
+from flask import Flask, render_template, request, url_for
+import movies.adapters.repository as repo
+from movies.adapters.memory_repository import MemoryRepository, populate
+
+
+def create_app(test_config=None):
+    """Construct the core application."""
+
+    app = Flask(__name__)
+
+    app.config.from_object('config.Config')
+    data_path = os.path.join('movies', 'adapters', 'data')
+
+    if test_config is not None:
+        app.config.from_mapping(test_config)
+        data_path = app.config['TEST_DATA_PATH']
+
+    repo.repo_instance = MemoryRepository()
+    populate(data_path, repo.repo_instance)
+
+    with app.app_context():
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
+
+        from .movie import movie
+        app.register_blueprint(movie.movie_blueprint)
+
+    return app
